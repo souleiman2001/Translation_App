0ecea8c4d79fc40779a8fd9b225a02c4960daaa8
Nameilous2022 <106993649+souleiman2001@users.noreply.github.com>
2023-10-25 02:15:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
+import os
 import re
 
 import nltk
+import requests
 from flask import Flask, render_template, request
+from googletrans import Translator
 from nltk.translate.bleu_score import sentence_bleu
 
+from finetuned_marian_120k import translate_with_finetuned
+from marefa_trans import translate_with_marefa
 from trans_test import \
     translate_text  # Ensure this module is in the same directory or adjust the import
+from turjuman_trans import translate_text_turjuman
 
 # Ensure the NLTK tokenizer model is available
 try:
@@ -15,6 +21,7 @@ except LookupError:
 
 
 app = Flask(__name__)
+translator = Translator()
 
 def compute_bleu(reference, candidate):
     # Tokenizing words in the reference and candidate sentences
@@ -63,6 +70,46 @@ def compare():
             score = compute_bleu(reference_text, translated_text)
     
     return render_template("compare.html", score=score, reference_text=reference_text, translated_text=translated_text)
+
+
+
+
+@app.route('/translate_gpt4', methods=['GET', 'POST'])
+def gpt4_translation_interface():
+    
+    if request.method == 'POST':
+        english_text = request.form.get('english_text')
+        translation = translator.translate(english_text, dest='ar')
+        return render_template('translate_gpt4.html', gpt4_translated_text=translation.text)
+    return render_template('translate_gpt4.html', gpt4_translated_text="")
+
+@app.route("/translate_turjuman", methods=["GET", "POST"])
+def translate_turjuman():
+    translation = ""
+    if request.method == "POST":
+        english_text = request.form.get("english_text", "")
+        translation = translate_text_turjuman(english_text)
+    return render_template("translate_turjuman.html", translation=translation)
+
+
+@app.route('/translate_marefa', methods=['GET', 'POST'])
+def translate_marefa():
+    translation = ""
+    if request.method == 'POST':
+        english_text = request.form['english_text']
+        translation = translate_with_marefa(english_text)
+    return render_template('marefa.html', translation=translation)
+
+@app.route("/translate_finetuned", methods=["GET", "POST"])
+def translate_finetuned():
+    translation = ""
+    if request.method == "POST":
+        english_text = request.form.get("english_text", "")
+        translation = translate_with_finetuned(english_text)
+    return render_template("fine_tuned_translation.html", translation=translation)
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
